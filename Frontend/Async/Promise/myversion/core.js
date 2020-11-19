cd65637816a8961fe73d7c97500fd7e311b22e93
xiangrong.liu_刘祥荣 <xiangrong.liu@wehotelglobal.com>
2020-11-19 16:51:28 +0800

    faet: hac

--- Frontend/Async/Promise/myversion/core.js
+++ Frontend/Async/Promise/myversion/core.js
@@ -3,7 +3,7 @@
  * @author: xiangrong.liu
  * @Date: 2020-11-12 19:10:00
  * @LastEditors: xiangrong.liu
- * @LastEditTime: 2020-11-18 18:09:00
+ * @LastEditTime: 2020-11-19 16:03:08
  */
 let LAST_ERROR = null;
 let IS_ERROR = {};
@@ -41,7 +41,7 @@ const noop = () => { }
 function Promise (fn) {
     this._status = 0
     this._value = null;
-    this._defferreds = null;
+    this._defferreds = [];
     Object.defineProperty(this, Symbol.toStringTag, {
         value: 'MyPromise'
     })
@@ -64,13 +64,13 @@ function Promise (fn) {
 
 Promise.prototype.then = function (onFullfilled, onRejected) {
     onFullfilled = onFullfilled ? onFullfilled : (data) => data
-    onRejected = onRejected ? onRejected : (e) => { console.log('haha'); throw e }
+    onRejected = onRejected ? onRejected : (e) => { throw e }
     let thenPromise = new Promise(noop);
-    this._defferreds = {
+    this._defferreds.push({
         thenPromise,
         onFullfilled,
         onRejected
-    }
+    })
     finale(this)
     return thenPromise;
 }
@@ -109,34 +109,56 @@ function reject (self, newVal) {
 
 function finale (self) {
     // console.log('final', self._defferreds);
-    if (!self._defferreds) {
+    if (!self._defferreds.length) {
         return
     }
 
-    // TODO: 应该是while，因为可能无限嵌套promise
+    // TODO: 应该是while，因为可能无限嵌套promise，先考虑一次
     if (self._status === 3) {
         self = self._value;
     }
 
     // TODO:为什么需要异步执行？
-    if (self._status === 1) {
-        let result = tryCallOne(self._defferreds.onFullfilled, self._value)
+    // if (self._status === 1) {
+    //     let result = tryCallOne(self._defferreds[0].onFullfilled, self._value)
+    //     if (result === IS_ERROR) {
+    //         reject(self._defferreds[0].thenPromise, LAST_ERROR)
+    //     } else {
+    //         resolve(self._defferreds[0].thenPromise, result)
+    //     }
+    // }
+    // if (self._status === 2) {
+    //     let result = tryCallOne(self._defferreds[0].onRejected, self._value)
+    //     if (result === IS_ERROR) {
+    //         reject(self._defferreds[0].thenPromise, LAST_ERROR)
+    //     } else {
+    //         // 注意，这里需要resolve
+    //         resolve(self._defferreds[0].thenPromise, result)
+    //     }
+    // }
+    let onFullfilled = self._defferreds[0].onFullfilled ? self._defferreds[0].onFullfilled : null;
+    let onRejected = self._defferreds[0].onRejected ? self._defferreds[0].onRejected : null;
+    let cb = self._status === 1 ? onFullfilled : onRejected
+
+    if (cb) {
+        let result = tryCallOne(cb, self._value)
         if (result === IS_ERROR) {
-            reject(self._defferreds.thenPromise, LAST_ERROR)
+            reject(self._defferreds[0].thenPromise, LAST_ERROR)
         } else {
-            resolve(self._defferreds.thenPromise, result)
+            resolve(self._defferreds[0].thenPromise, result)
         }
+    } else {
+        
     }
-    if (self._status === 2) {
-        let result = tryCallOne(self._defferreds.onRejected, self._value)
-        if (result === IS_ERROR) {
-            reject(self._defferreds.thenPromise, LAST_ERROR)
-        } else {
-            // 注意，这里需要resolve
-            resolve(self._defferreds.thenPromise, result)
-        }
-    }
+    // self._defferreds.forEach(defferred => {
+    //     // TODO: 这里的this应该传什么
+    //     handleResolved(self, defferred)
+    // });
 }
+
+// function handleResolved(self, defferred) {
+
+// }
 
 var promise1 = new Promise((resolve, reject) => {
     // resolve(1000)
@@ -148,7 +170,9 @@ var promise1 = new Promise((resolve, reject) => {
     // setTimeout(() => {
     //     reject(2000)
     // }, 1000)
+    // resolve(new Promise(() => {}))
     resolve(new Promise((resolve2, reject2) => {resolve2(3000)}))
+    // resolve(new Promise((resolve2, reject2) => {resolve2(3000)}).then(_res => {console.log(_res)}));
 })
 
 // promise1.then((res) => {
--- Frontend/Async/Promise/sourcecode/demo/embed.js
+++ Frontend/Async/Promise/sourcecode/demo/embed.js
@@ -3,7 +3,7 @@
  * @author: xiangrong.liu
  * @Date: 2020-10-30 21:34:41
  * @LastEditors: xiangrong.liu
- * @LastEditTime: 2020-11-09 18:14:25
+ * @LastEditTime: 2020-11-19 15:54:54
  */
 const Promise = require('../src/core');
 
@@ -87,5 +87,27 @@ function promiseTest5 () {
     })
 }
 
-promiseTest1()
+function promiseTest6() {
+    let promise = new Promise((resolve, reject) => {
+        resolve(new Promise((resolve2, reject2) => {
+            resolve2(1000)
+        }).then(res0 => {
+            console.log('res0', res0)
+        }, err0 => {
+            console.log('err0', err0)
+        }))
+    })
+    promise.then(res1 => {
+        console.log('res1', res1)
+    }, err1 => {
+        console.log('err1', err1)
+    }).then(res2 => {
+        console.log('res2', res2)
+    }, err2 => {
+        console.log('err2', err2)
+    })
+}
+
+// promiseTest1()
 // promiseTest2()
+promiseTest6()
